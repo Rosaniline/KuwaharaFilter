668fdacfbae79223795d79119a5dde51c76e40ee
Rosani Lin <xup6qup3@gmail.com>
2012-08-03 14:18:49 +0800

    Initial Commit

---
+++ Kuwahara Filter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Kuwahara Filter
+//
+//  Created by Rosani Lin on 12/8/3.
+//  Copyright (c) 2012年 Rosani Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
